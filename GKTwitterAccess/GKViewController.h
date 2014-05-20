b67d07e2c59d8144dc38243214d07d8947e0e4c9
Georg Kitz <georgkitz@gmail.com>
2014-05-20 10:29:17 +0200

    Initial Commit

---
+++ GKTwitterAccess/GKViewController.h
@@ -0,0 +1,13 @@
+//
+//  GKViewController.h
+//  GKTwitterAccess
+//
+//  Created by Georg Kitz on 20/05/14.
+//  Copyright (c) 2014 Aurora Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GKViewController : UIViewController
+
+@end
---
+++ GKTwitterAccess/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GKTwitterAccess
+//
+//  Created by Georg Kitz on 20/05/14.
+//  Copyright (c) 2014 Aurora Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GKAppDelegate class]));
+    }
+}
